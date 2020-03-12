8e23a7a1f167f3f5d47c64f6376ecae2db6283a3
mgarlickk <38113893+mgarlickk@users.noreply.github.com>
2020-03-12 16:47:46 -0400

    t

---
+++ OneMonth/Javascript-Course/Week2/js/main.js
@@ -0,0 +1,20 @@
+
+/* Grab the input */
+
+
+
+document.getElementById('js-search').addEventListener('click', function(){
+    
+    var inputText = document.getElementById('input-field').value;
+    var gifContainer = document.getElementById('js-gif-container').innerHTML = inputText;
+    
+    
+});
+
+
+/* Do the data work with the API */
+
+
+
+
+/* Show the GIFS */
