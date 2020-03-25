dbb41e7675d13977a93125b9d8c666ddba77488c
mgarlickk <38113893+mgarlickk@users.noreply.github.com>
2020-03-25 18:40:53 -0400

    t

--- OneMonth/Javascript-Course/Week2/js/main.js
+++ OneMonth/Javascript-Course/Week2/js/main.js
@@ -1,5 +1,5 @@
 
-/* Grab the input */
+/* 1. Grab the input */
 
 document.getElementById('js-search').addEventListener('click', function(){
     var inputText = document.getElementById('input-field').value;
@@ -17,43 +17,38 @@ document.getElementById('input-field').addEventListener('keyup', function(e){
 });
 
 
-/* Do the data work with the API */
+/* 2. Do the data work with the API */
 
-var shaqUrl = "http://api.giphy.com/v1/gifs/search?q=shaq&api_key=dc6zaTOxFJmzC";
+var trendingUrl = "https://api.giphy.com/v1/gifs/trending?api_key=STOPsEqOA089eUeZvDCj5n9Fz3snySCW&limit=25&rating=G";
+var catsUrl = "http://api.giphy.com/v1/gifs/search?q=cats&api_key=dc6zaTOxFJmzC";
 var dogsUrl = "http://api.giphy.com/v1/gifs/search?q=dogs&api_key=dc6zaTOxFJmzC";
 
 var GiphyAJAXCall = new XMLHttpRequest();
-GiphyAJAXCall.open('GET', shaqUrl);
+GiphyAJAXCall.open('GET', trendingUrl);
 GiphyAJAXCall.send();
 
 GiphyAJAXCall.addEventListener('load', function(e){
-    
     var data = e.target.response;
     pushToDOM(data);
 });
 
 
-/* Show the GIFS */
+/* 3. Show the GIFS */
 
 function pushToDOM(inputText){
     
-    if (inputText==="cats"){
-        alert('blah');
-    }
-    
+    // Clean up data into an object
     var response = JSON.parse(inputText);
     
     var imageUrls = response.data;
     
     imageUrls.forEach(function(image){
         var src = image.images.fixed_height.url;
-        var gifContainer = document.getElementById('js-gif-container');
-        
-        gifContainer.innerHTML += "<img src=\"" + src +"\" class=\"gifs\">";
-        
         console.log(src);
+        var gifContainer = document.getElementById('js-gif-container');
+        element = gifContainer.innerHTML += "<img src=\""+src+"\" class=\"gifs\">";
     });
-    
+                
 }
 
 
