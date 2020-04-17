60e29ef5d47dfd01eefb64afc855ef173721ff8d
mgarlick12 <mgarlickk@gmail.com>
2020-04-16 21:24:40 -0400

    test

--- Javascript-Course/Week2/js/main.js
+++ Javascript-Course/Week2/js/main.js
@@ -2,49 +2,51 @@
 /* 1. Grab the input */
 
 document.getElementById('js-search').addEventListener('click', function(){
-    var inputText = document.getElementById('input-field').value;
-    pushToDOM(inputText);
+    var inputText = document.getElementById('js-user-input').value;
+    var userInput = getUserInput();
+    searchGiphy(userInput);
 });
 
-document.getElementById('input-field').addEventListener('keyup', function(e){
-    var inputText = document.getElementById('input-field').value;
-    console.log(inputText);
-    
+document.getElementById('js-user-input').addEventListener('keyup', function(e){
     // if the key ENTER is pressed...
     if(e.which===13){
-       pushToDOM(inputText);
+        var userInput = getUserInput();
+        searchGiphy(userInput);
     }
 });
+
+function getUserInput(){
+    var inputValue = document.getElementById('js-user-input').value;
+    return inputValue;
+}
 
 
 /* 2. Do the data work with the API */
 
-var trendingUrl = "https://api.giphy.com/v1/gifs/trending?api_key=STOPsEqOA089eUeZvDCj5n9Fz3snySCW&limit=25&rating=G";
-var catsUrl = "http://api.giphy.com/v1/gifs/search?q=cats&api_key=dc6zaTOxFJmzC";
-var dogsUrl = "http://api.giphy.com/v1/gifs/search?q=dogs&api_key=dc6zaTOxFJmzC";
+function searchGiphy(searchQuery){
+    var url = "https://api.giphy.com/v1/gifs/search?&api_key=STOPsEqOA089eUeZvDCj5n9Fz3snySCW&q="+searchQuery;
+    
+    // AJAX Request
+    var GiphyAJAXCall = new XMLHttpRequest();
+    GiphyAJAXCall.open('GET', url);
+    GiphyAJAXCall.send();
 
-
-var GiphyAJAXCall = new XMLHttpRequest();
-GiphyAJAXCall.open('GET', trendingUrl);
-GiphyAJAXCall.send();
-
-GiphyAJAXCall.addEventListener('load', function(e){
-    var data = e.target.response;
-    pushToDOM(data);
+    GiphyAJAXCall.addEventListener('load', function(data){
+        var actualData = data.target.response;
+        pushToDOM(actualData);
+        console.log(actualData);
 });
+
+}
 
 
 /* 3. Show the GIFS */
 
-function pushToDOM(inputText){
-    
-    if (inputText==='cats'){
-        alert('testing');
-    }
+function pushToDOM(actualData){
     
     // Clean up data into an object
-    var response = JSON.parse(inputText);
-    
+    var response = JSON.parse(actualData);
+    console.log(response);
     var imageUrls = response.data;
     
     imageUrls.forEach(function(image){
@@ -52,7 +54,7 @@ function pushToDOM(inputText){
         var gifContainer = document.getElementById('js-gif-container');
         gifContainer.innerHTML += "<img src=\""+src+"\" class=\"gifs\">";
     });
-                
+       
 }
 
 
---
+++ Javascript-Course/Week3/javascript/main.js
@@ -0,0 +1,30 @@
+/* 1. Search */
+
+
+
+
+
+/* 2. Query Soundcloud API */
+
+
+SC.initialize({
+  client_id: 'cd9be64eeb32d1741c17cb39e41d254d'
+});
+
+// find all sounds of buskers licensed under 'creative commons share alike'
+SC.get('/tracks', {
+  q: 'blink-182', titl: 'all the small things'
+}).then(function(tracks) {
+  console.log(tracks);
+});
+
+
+
+/* 3. Display the cards */
+
+
+
+
+
+
+/* 4. Add to playlist and play */
