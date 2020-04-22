bd5b4d2ed6b0fe1ebc8d0af536b8b3575de2792f
mgarlick12 <mgarlickk@gmail.com>
2020-04-22 19:14:49 -0400

    test

--- Javascript-Course/Week3/javascript/main.js
+++ Javascript-Course/Week3/javascript/main.js
@@ -9,13 +9,13 @@
 
 var SoundCloudAPI = {}
 
-
 SoundCloudAPI.init = function(){
     SC.initialize({
         client_id: 'cd9be64eeb32d1741c17cb39e41d254d'
     });
 };
 
+SoundCloudAPI.init();
 
 SoundCloudAPI.getTrack = function(inputValue){
     // find all sounds of buskers licensed under 'creative commons share alike'
@@ -31,8 +31,24 @@ SoundCloudAPI.getTrack('zedd');
 
 /* 3. Display the cards */
 
+var card = document.createElement('div');
+card.classList.add('card');
 
+var addCard = document.querySelector('.js-search-results');
+addCard.appendChild(card);
 
+var imgDiv = document.createElement('div');
+imgDiv.classList.add('image');
+imgDiv.classList.add('testing');
+
+var addImgDiv = document.querySelector('.card:nth-child(2)');
+addImgDiv.appendChild(imgDiv);
+
+var catImage = document.createElement('div');
+catImage.classList.add('image_img');
+
+var addCatImage = document.querySelector('.testing');
+addImgDiv.appendChild(imgDiv);
 
 
 
