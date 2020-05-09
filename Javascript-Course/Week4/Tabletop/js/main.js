3df5fac48edd00f34b6ee272266c0172c0138481
mgarlick12 <mgarlickk@gmail.com>
2020-05-08 22:36:18 -0400

    test

--- Javascript-Course/Week4/Tabletop/js/main.js
+++ Javascript-Course/Week4/Tabletop/js/main.js
@@ -1,22 +1,23 @@
-var publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1qqMY9Mm0tEY-kmq2E9EfwS6-Z1ZoRk9pF7zK78SdzTQ/edit?usp=sharing';
+//var publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1qqMY9Mm0tEY-kmq2E9EfwS6-Z1ZoRk9pF7zK78SdzTQ/edit?usp=sharing';
+var publicWoWSpreadsheet = 'https://docs.google.com/spreadsheets/d/1dcnjO261YSfoUh1L3EcwN9NlPpDZDQND9CqyEYd_Z-M/edit?usp=sharing';
 
-function init() {
-Tabletop.init( { key: publicSpreadsheetUrl,
-                 callback: showInfo,
-                 simpleSheet: true } )
-}
+  function init() {
+    Tabletop.init( {
+      key: publicWoWSpreadsheet,
+      callback: showInfo,
+      simpleSheet: true 
+    })
+  }
 
-function showInfo(data, tabletop) {
-console.log(data);
-}
 
-window.addEventListener('DOMContentLoaded', init);
-
-function showInfo(data, tabletop){
-    data.forEach(function(data){
+    function showInfo(data, tabletop){
+        data.forEach(function(data){
         var header = document.querySelector('.header');
         var newDiv = document.createElement('div');
         header.append(newDiv);
         newDiv.innerHTML = data.Category;
-    });
-}
+        console.log(data.Category);
+        })
+    }
+
+window.addEventListener('DOMContentLoaded', init);
