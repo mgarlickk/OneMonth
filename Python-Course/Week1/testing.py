86f46943fb409973ed1b71d1a6323e255276dbee
mgarlickk <38113893+mgarlickk@users.noreply.github.com>
2020-05-18 20:43:56 -0400

    t

--- Python-Course/Week1/testing.py
+++ Python-Course/Week1/testing.py
@@ -1,1 +1,30 @@
-print('testing')
+import random
+
+bars = ["Schoolbred's",
+        "The Wren",
+        "The Scratcher",
+        "ACME",
+        "Blind Barber",
+        "Testing"]
+
+people = ["Mattan",
+        "Chris",
+        "that person",
+        "kanye west",
+        "Samuel L. Jackson"]
+
+random_bar = random.choice(bars)
+random_person_one = random.choice(people)
+random_person_two = random.choice(people)
+
+
+if random_person_one == random_person_two:
+  people_equal = True
+  while people_equal == True:
+    random_person_one = random.choice(people)
+    break
+
+
+
+print(f"How about you go to {random_bar} with " + random_person_one + ' and ' + random_person_two)
+
