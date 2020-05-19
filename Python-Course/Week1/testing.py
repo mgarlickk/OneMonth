5ec045255d7beaf212db298b6157944e43a426d4
mgarlick12 <mgarlickk@gmail.com>
2020-05-19 00:06:43 -0400

    t

--- Python-Course/Week1/testing.py
+++ Python-Course/Week1/testing.py
@@ -17,14 +17,14 @@ random_bar = random.choice(bars)
 random_person_one = random.choice(people)
 random_person_two = random.choice(people)
 
-
 if random_person_one == random_person_two:
   people_equal = True
   while people_equal == True:
     random_person_one = random.choice(people)
     break
 
-
+for x in people:
+        print(x)
 
 print(f"How about you go to {random_bar} with " + random_person_one + ' and ' + random_person_two)
 
