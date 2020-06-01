8c09d104d8360fd888f1cae0fee18d394da84ca0
mgarlickk <38113893+mgarlickk@users.noreply.github.com>
2020-06-01 12:02:03 -0400

    test

--- Python-Course/Week4/hello.py
+++ Python-Course/Week4/hello.py
@@ -1,6 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello world"
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contacts')
+def contacts():
+    return render_template('contacts.html')
