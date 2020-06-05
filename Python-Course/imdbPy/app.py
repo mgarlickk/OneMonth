e983427d17e17344dcd2db803b3e6d83effedc78
mgarlickk <38113893+mgarlickk@users.noreply.github.com>
2020-06-05 18:23:42 -0400

    test

--- Python-Course/imdbPy/app.py
+++ Python-Course/imdbPy/app.py
@@ -9,26 +9,30 @@ movie = ia.get_movie('0133093')
 directors_list = movie['directors']
 top = ia.get_top250_movies()
 
+_keywordmovies = ia.get_keyword('crime')
+
 
 @app.route('/')
 def index():
     return render_template('index.html', movie=movie)
 
-@app.route('/directors')
+@app.route('/directors', methods=['POST', 'GET'])
 def directors():
-    return render_template('directors.html', directors=directors_list)
+    if request.method == "POST":
+        directors = request.form['nm']
+        keywords = ia.search_keyword(directors)
+        return render_template('directors.html', directors=keywords)
+    else:
+        return 'nothing'
 
 @app.route('/top_250')
 def top_250():
     return render_template('top_250.html', top_250=top)
 
-@app.route('/genre')
-def genres():
-    return render_template('genres.html', genres=genres)
-
 @app.route('/genres')
-def json():
-    return render_template('genres.html')
+def genres():
+    return render_template('genres.html', genres=genres, keywords=_keywordmovies)
+
 
 
 
